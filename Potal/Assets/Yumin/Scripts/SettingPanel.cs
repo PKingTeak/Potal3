9b07d866a9b5858078a6d7477a218aca267c1096
Renew023 <ya1539@naver.com>
2025-05-26 20:49:09 +0900

    feat : StartSceneUI 제작
    feat : StartSceneUI 제작
    - StartButton
    - SettingButton
    - MapBuildButton(아직 제작 X)

---
+++ Potal/Assets/Yumin/Scripts/SettingPanel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingPanel : MonoBehaviour
+{
+	[SerializeField] private StartSceneUI startSceneUI;
+
+	[SerializeField] private Slider soundSlider;
+	[SerializeField] private Slider SFXSlider;
+	[SerializeField] private Button closeButton;
+	//필요 : 배경음악
+	//필요 : 효과음
+
+	private void Awake()
+	{
+		soundSlider.onValueChanged.AddListener(OnSoundSliderChanged);
+		SFXSlider.onValueChanged.AddListener(OnSFXSliderChanged);
+		closeButton.onClick.AddListener(() => startSceneUI.SetActivePanel(UIState.Main));
+	}
+
+	private void OnSoundSliderChanged(float value)
+	{
+		// 필요 : 배경음악.볼륨
+		Debug.Log($"Sound volume changed to: {value}");
+	}
+	private void OnSFXSliderChanged(float value)
+	{
+		// 필요 : 효과음. 볼륨
+		Debug.Log($"SFX volume changed to: {value}");
+	}
+}
