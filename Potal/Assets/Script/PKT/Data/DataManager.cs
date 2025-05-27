b474b9baac623c6df0aee31ae44ee6c71d219b6a
PKT <rudxorrmrwkd@gmail.com>
2025-05-28 00:25:23 +0900

    feat : 여러 데이터 처리 해보기 성공

--- Potal/Assets/Script/PKT/Data/DataManager.cs
+++ Potal/Assets/Script/PKT/Data/DataManager.cs
@@ -1,33 +1,47 @@
 using SW;
-using System.Collections;
+
 using System.Collections.Generic;
-using System.Linq;
+
 using UnityEngine;
-using UnityEngine.Purchasing.MiniJSON;
 
 
-public class DataManager 
+
+public class DataManager
 {
         
     List<StageData> datas = new List<StageData>();
     Dictionary<int, StageData> dataDict = new Dictionary<int, StageData>();
 
+    Dictionary<int, StageData> MydataDict = new Dictionary<int, StageData>();
+
+   
     public void JsonToData()
     {
 
        string json =  Resources.Load<TextAsset>("Json/StageData/Stage02").text;
         //LoadAll로 변경
 
-        
+        //영튜한테 질문드리기
+        TextAsset[] jsons = Resources.LoadAll<TextAsset>("Json/StageData");
+
 
         //뭘해겠어? 텍스트를 이제 구분지어서 리스트로 담아야겠지??
         //하나만 가져올꺼야
         StageData data =JsonUtility.FromJson<StageData>(json);
+
+        for (int i = 0; i < jsons.Length; i++)
+        {
+            StageData data2 = JsonUtility.FromJson<StageData>(jsons[i].ToString());
+            datas.Add(data2); //
+          //  Debug.Log($"이름 : {data2.PrefabEntries}+ {data2.PrefabEntries[0].prefabPath}");
+        }
+
+
         //값을 가져옴
-        datas.Add(data);
         for (int i = 0; i < datas.Count; i++)
         {
             dataDict.Add(i,datas[i]); //캐싱
+            
         
         }
 
