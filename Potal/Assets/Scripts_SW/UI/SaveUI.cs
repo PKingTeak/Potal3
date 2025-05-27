1c971bf37337d84db6c0ecc43b0f6ed425a3772c
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-27 19:06:52 +0900

    feat: SaveUI 추가

---
+++ Potal/Assets/Scripts_SW/UI/SaveUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using SW;
+using UnityEngine;
+
+public class SaveUI : MonoBehaviour
+{
+    [SerializeField]
+    private SelectedListViewUI selectedListViewUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
