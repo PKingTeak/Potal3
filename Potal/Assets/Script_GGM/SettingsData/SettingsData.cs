aff2c6442e8366bf7d7e55f450c7b08706e4acc4
rudals4469 <80159080+rudals4469@users.noreply.github.com>
2025-05-27 12:19:11 +0900

    feat : SettingData

---
+++ Potal/Assets/Script_GGM/SettingsData/SettingsData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SoundSettingsData
+{
+    public float soundVolume = 1f;
+    public float sfxVolume = 1f;
+    public float mouseSensitivity = 1f;
+}
+
