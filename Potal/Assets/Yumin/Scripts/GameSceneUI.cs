030e824bf696f095357a1297a9fae9d99fc19fd1
Renew023 <ya1539@naver.com>
2025-05-27 12:51:50 +0900

    feat : UI 마우스 감도 추가 및 표기 텍스트 추가

--- Potal/Assets/Yumin/Scripts/GameSceneUI.cs
+++ Potal/Assets/Yumin/Scripts/GameSceneUI.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameSceneUI : MonoBehaviour
 {
-    [Header("잡담 표시")]
+    [SerializeField] private PlayerMovement playerInput; 
+												//플레이어 isMove 값 필요. 
+	[Header("잡담 표시")]
     [SerializeField] private TextMeshProUGUI promptText;
     
     [Header("상호작용 시 표시")]
     [SerializeField] private GameObject interactPanel;
     [SerializeField] private TextMeshProUGUI keyText;
 	[SerializeField] private TextMeshProUGUI actionNameText;
+
+    [Header("패널 On, Off")]
+    [SerializeField] private GameObject diePanel; // 죽으면 표시
+    [SerializeField] private GameObject clearPanel; // 끝 부분에 도달하면 표시
+    [SerializeField] private GameObject settingPanel;
 
     public void GetInteractData(string tag = "None")
     {
@@ -28,4 +36,19 @@ public class GameSceneUI : MonoBehaviour
 				break;
 		}
     }
+
+    public void OnSetting(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            settingPanel.SetActive(true);
+            OpenUI(!settingPanel.activeSelf);
+		}
+    }
+
+    public void OpenUI(bool isOpen)
+    {
+        Cursor.lockState = isOpen == true ? CursorLockMode.Locked : CursorLockMode.None;
+        playerInput.enabled = isOpen; // 플레이어 입력 비활성화
+	}
 }
