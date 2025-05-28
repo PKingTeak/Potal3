95deba655b69bce20bd6849336e717dbea86b778
qwe1e34r56ty <sunwoonana@gmail.com>
2025-05-28 12:09:19 +0900

    feat: MakeStageScene -> 타 Scene 연결 추가

---
+++ Potal/Assets/Script/Stage/MakeStage/UI/ToOtherSceneUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SW {
+    public class ToOtherSceneUI : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+
+        public void onGoToMainButtonClick()
+        {
+            SceneManager.LoadScene("StartScene");
+        }
+    }
+}
