6b0da0dab040a6ac70752c258a439b0992838fcc
Seongeun Baek <100seongeun@gmail.com>
2025-05-29 10:36:03 +0900

    feat : 포탈 이동 시 복제 기능 추가

--- Potal/Assets/Script/Potal/Portal.cs
+++ Potal/Assets/Script/Potal/Portal.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     [Header("Portal Settings")]
-    public Transform viewPortal;
-    public Portal linkedPortal;
-    public MeshRenderer screen;
-    public Camera portalCamera;
+    [SerializeField]
+    private Portal linkedPortal;
+
+    [SerializeField]
+    private Camera portalCamera;
 
     private float maxDistance = 10f;
 
@@ -16,92 +17,133 @@ public class Portal : MonoBehaviour
     [SerializeField]
     private Transform player;
 
-    void Start()
+    private void Update()
     {
-        portalCamera.enabled = true;
+        if (travellers.Count > 0)
+        {
+            CheckTravellers();
+        }
     }
 
-    void LateUpdate()
+    private void LateUpdate()
     {
         UpdatePortalCamera();
     }
 
-    void UpdatePortalCamera()
+    private void UpdatePortalCamera()
     {
-        float distance = Vector3.Distance(player.position, viewPortal.position); // 플레이어와 포탈 사기 계산
+        // 플레이어와 포탈 사기 계산
+        float distance = Vector3.Distance(player.position, transform.position);
 
         float targetFOV = Mathf.Lerp(60f, 100f, distance / maxDistance);
 
-        portalCamera.fieldOfView = targetFOV; // 포탈 카메라 fieldOfView 적용
+        // 포탈 카메라 fieldOfView 적용
+        portalCamera.fieldOfView = targetFOV;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<PortalTraveller>(out var traveller))
         {
-            Debug.Log("포탈에 뭔가 들어온게 감지");
             OnTravellerEnterPortal(traveller);
-            foreach (var t in travellers)
-            {
-                Debug.Log("현재 들어온 것들" + t.name);
-            }
         }
     }
 
-    public void OnTravellerEnterPortal(PortalTraveller traveller)
+    // 포탈뒤에 있는 벽들의 Collider 모두 반환
+    private List<Collider> GetWallBehindPortal()
     {
-        if (!travellers.Contains(traveller))
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, -transform.forward, 0.5f);
+
+        List<Collider> colliders = new List<Collider>();
+
+        foreach (RaycastHit hit in hits)
         {
-            travellers.Add(traveller);
+            if (hit.collider != null)
+                colliders.Add(hit.collider);
         }
+
+        return colliders;
     }
 
-    private void OnTriggerStay(Collider other)
+    // traveller가 포탈에 들어왔을 때 처리
+    private void OnTravellerEnterPortal(PortalTraveller traveller)
     {
-        if (!other.CompareTag("Player") && !other.CompareTag("test"))
-            return;
+        // 리스트에 없으면 추가
+        if (!travellers.Contains(traveller))
+            travellers.Add(traveller);
 
+        // 클론 없으면 생성, 있으면 활성화만
+        if (traveller.clone == null)
+        {
+            traveller.clone = Instantiate(traveller.clonePrefab);
+            traveller.clone.name = traveller.name + "_Clone";
+            traveller.clone.SetActive(true);
+        }
+        traveller.clone.SetActive(true);
+
+        // 클론 배치
+        traveller.UpdateCloneTransform(transform, linkedPortal.transform);
+    }
+
+    // 포탈 안에 있는 traveller 위치 감지, 이동 처리
+    private void CheckTravellers()
+    {
         for (int i = 0; i < travellers.Count; i++)
         {
             PortalTraveller traveller = travellers[i];
-            Vector3 offset = traveller.transform.position - transform.position; // 이동 오브젝트 위치 계산
-            float dot = Vector3.Dot(transform.forward, offset); // 오브젝트가 앞인지 뒤인지 판별
-            // Debug.Log(dot); // dot확인 디버그
-            if (dot < 0.5f)
+
+            // 클론 위치 계속 갱신
+            if (traveller.clone != null && traveller.clone.activeSelf)
             {
-                traveller.Teleport(transform, traveller.transform, linkedPortal.transform);
+                traveller.UpdateCloneTransform(transform, linkedPortal.transform);
+            }
+
+            Vector3 offset = traveller.transform.position - transform.position; // traveller 위치 계산
+            float dot = Vector3.Dot(transform.forward, offset); // traveller가 앞인지 뒤인지 판별
+
+            // 벽과 traveller의 충돌 제거
+            SetWallCollision(traveller, true);
+            if (dot < 0f)
+            {
+                // 본체를 클론 위치로 이동시키고, 상대 포탈에 클론 배치
+                traveller.Teleport();
+                // 포탈 도착 지점에 이동했을때 시작지점에 클론 배치
                 linkedPortal.OnTravellerEnterPortal(traveller);
-                travellers.RemoveAt(i);
-                i--;
+                if (traveller.clone != null)
+                    traveller.clone.SetActive(false);
             }
         }
-
-        // if (dot < 0.5f)
-        // {
-        //     // 현재 포탈 기준으로 플레이어 위치 로컬 좌표로 변환
-        //     Vector3 relativePos = transform.InverseTransformPoint(player.position);
-        //     // 반대쪽 포탈 공간으로 위치로 이동, 0.6정도 더 이동
-        //     Vector3 newPos =
-        //         linkedPortal.transform.TransformPoint(relativePos)
-        //         + linkedPortal.transform.forward * 0.4f;
-
-        //     // 현재 포탈 기준으로 플레이어의 회전값 계산
-        //     Quaternion relativeRot = Quaternion.Inverse(transform.rotation) * player.rotation;
-        //     // 이동한 포탈 기준으로 플레이어의 회전값 계산
-        //     Quaternion newRot = linkedPortal.transform.rotation * relativeRot;
-
-        //     // 방향 보정(포탈 뒤에서 나오니까)
-        //     newRot *= Quaternion.Euler(0, 180f, 0);
-
-        //     // 플레이어 위치값과 회전값 적용
-        //     player.SetPositionAndRotation(newPos, newRot);
-        // }
     }
 
+    // traveller와 포탈 뒤쪽 벽 충돌 설정
+    private void SetWallCollision(PortalTraveller traveller, bool ignore)
+    {
+        Collider travellerCol = traveller.GetComponent<Collider>();
+        List<Collider> wallCols = GetWallBehindPortal();
+
+        if (wallCols != null && travellerCol != null)
+        {
+            foreach (Collider wallCol in wallCols)
+            {
+                Physics.IgnoreCollision(travellerCol, wallCol, ignore);
+            }
+        }
+    }
+
+    // 포탈 벗어났을 시
     private void OnTriggerExit(Collider other)
     {
-        if (!other.CompareTag("Player"))
-            return;
-        // 플레이어가 포탈에서 나갔을 때 다시 포탈 사용가능하게 설정F
+        if (other.TryGetComponent<PortalTraveller>(out var traveller))
+        {
+            if (traveller.clone != null)
+                traveller.clone.SetActive(false);
+
+            travellers.Remove(traveller);
+
+            if (travellers.Count == 0)
+            {
+                SetWallCollision(traveller, false);
+            }
+        }
     }
 }
