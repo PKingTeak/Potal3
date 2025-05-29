2c5f0d22679179276bb89e7428b52666c6607a70
PKT <rudxorrmrwkd@gmail.com>
2025-05-29 14:36:41 +0900

    문 연동 완료 근데 조금 이상한부분 있음

--- Potal/Assets/Script/Stage/StageSettingHelper.cs
+++ Potal/Assets/Script/Stage/StageSettingHelper.cs
@@ -10,13 +10,16 @@ public class StageSettingHelper : MonoBehaviour
     public static event Action onCompleted;
 
 
-    public StageDataManager dataManager;
+    public StageDataLoader dataLoader;
+
+    private int buttonid;
+    private int doorid;
 
     private void Awake()
     {
-        dataManager = new StageDataManager();
-        dataManager.JsonToData();
-                      
+        dataLoader = new StageDataLoader();
+        dataLoader.JsonToData();
+        
     }
 
 
@@ -25,16 +28,34 @@ public class StageSettingHelper : MonoBehaviour
         foreach(var map in data.PrefabEntries)
         {
             GameObject go = Resources.Load<GameObject>($"Prefabs/RealStagePrefab/{map.prefabPath}");
-            GameObject entryGo = Instantiate(go);
-            go.transform.position = map.position;
+            Vector3 goPosition = map.position;
+            Quaternion goRotaion = Quaternion.Euler( map.rotation);
+
+            GameObject entryGo = Instantiate(go,goPosition, goRotaion);
+            if (map.prefabPath == "Button")
+            {
+                entryGo.GetComponent<DoorButton>().SetId(buttonid);
+                //짝지어주는거 조금 고민해야될듯
+                //이건 데이터에 부여해야 될듯하다. 
+                buttonid++;
+            }
+            else if (map.prefabPath == "ButtonDoor")
+            {
+
+                entryGo.GetComponent<Door>().SetId(doorid);
+                doorid++;
+            }
+            
+            //버튼 연결은 추후에 해야한다. 모든 오브젝트가 달리고 버튼을 셋팅해줘야한다. 
         }
+
 
         onCompleted?.Invoke();
     }
 
     public void FindmapIndex(int stage)
     {
-        SettingMap( dataManager.GetStageData(stage));
+        SettingMap(dataLoader.GetStageData(stage));
     }
 
    
